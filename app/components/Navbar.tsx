8fe5dfeb3a5351f1ce2862d09a57ed1e3e59989a
Kamsi <kamsi_orazulike@icloud.com>
2024-11-28 23:47:17 +0000

    removed links from nav for now

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,6 @@ const Navbar = () => {
   return (
     <nav className="fixed w-full bg-black p-3 md:p-6">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        {/* Logo/App Name */}
         <Link href="/" className="text-2xl font-semibold text-white">
           <Image
             width={80}
@@ -15,22 +14,6 @@ const Navbar = () => {
             alt="logo"
           />
         </Link>
-
-        {/* Navigation Links */}
-        <div className="space-x-8">
-          <Link
-            href="#"
-            className="text-white/60 hover:text-white transition-colors"
-          >
-            How it works
-          </Link>
-          <Link
-            href="#"
-            className="text-white font-bold bg-gradient-to-r hover:bg-gradient-to-l from-[#02F199] to-[#56A6FF] px-10 py-3 rounded-lg transition-all duration-200"
-          >
-            Join Waitlist
-          </Link>
-        </div>
       </div>
     </nav>
   );
