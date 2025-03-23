b39da6a1f093b18c334bd3025821ebba76128a21
Kamsi <kamsi_orazulike@icloud.com>
2025-03-23 16:07:49 +0000

    vercel security fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="fixed w-full text-center mx-auto bottom-0 bg-transparent">
-      <footer className="px-14 py-4 text-center">© 2025 Rivals</footer>
+    <div className="fixed w-full text-left mx-auto bottom-0 bg-transparent">
+      <footer className="px-14 py-4 text-xs text-center">© 2025 Rivals</footer>
     </div>
   );
 };
