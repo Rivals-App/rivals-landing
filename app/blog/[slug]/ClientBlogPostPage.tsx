8cd54f32a3fb85a0c38187b692313317e6d3dccf
Kamsi <kamsi_orazulike@icloud.com>
2025-04-09 06:10:53 +0100

    eslint fixes

--- app/blog/[slug]/ClientBlogPostPage.tsx
+++ app/blog/[slug]/ClientBlogPostPage.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import React, { useEffect, useRef } from "react";
