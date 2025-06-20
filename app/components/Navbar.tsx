46e4d66794d60a267a8cb932abc97f32ea33df57
Joshua Jones <jonesryaljoshua@gmail.com>
2025-06-20 11:25:17 -0700

    Link FIx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
-import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import Image from "next/image";
 import JoinWaitlistButton from "./JoinWaitlistButton";
